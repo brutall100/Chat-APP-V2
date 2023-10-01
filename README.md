b148cfd5c344b27a63844a8053e5f5009d3146fc
brutall100 <37956008+brutall100@users.noreply.github.com>
2023-10-01 14:39:40 +0300

    V-1.0.0
    Working chat app with database conection.

--- README.md
+++ README.md
@@ -1,2 +1,77 @@
-# Chat App V2
- 
+# Chat App README
+Here's a README.md file that explains how your HTML, client.js, and server.js code work together.
+
+This README provides an overview of a simple chat application that allows users to send and receive messages through a web interface. The application consists of HTML, client-side JavaScript (client.js), and server-side JavaScript (server.js).
+
+## Table of Contents
+
+- [Features](#features)
+- [Getting Started](#getting-started)
+- [How It Works](#how-it-works)
+- [Dependencies](#dependencies)
+- [Authors](#authors)
+
+## Features
+- Users can input messages in an input field.
+- Sent messages are displayed in real-time on the web page.
+- Messages are sent to the server and inserted into a MySQL database.
+
+## Getting Started
+Before running the application, ensure you have the following installed:
+
+- Node.js: [Download and install Node.js](https://nodejs.org/).
+- XAMPP: [Download and install XAMPP](https://www.apachefriends.org/index.html).
+
+Install all the necessary Node.js packages and dependencies for your Express.js application, including express, body-parser, mysql2, dotenv, and cors, you can use the following commands in your project directory:
+
+
+
+### Installation
+
+npm install express body-parser mysql2 dotenv cors
+
+
+Create a .env file in the root directory and add your MySQL database configuration:
+Inside .env add this.
+# Database 
+DB_HOST=localhost
+DB_USER=root
+DB_PASSWORD=
+DB_DATABASE=chat
+# Port 
+PORT=3000
+
+
+Start the server: node server.js
+The server will start listening on the specified port.
+
+
+# How It Works
+# HTML (index.html)
+Provides the structure for the web page, including a chat container and input form.
+Loads the client-side JavaScript (client.js) and styles (styles.css).
+# Client-Side JavaScript (client.js)
+Handles user interaction and communication with the server.
+Defines functions:
+addMessage(messageText): Adds a new message to the chat interface.
+sendMessageToServer(messageText): Sends a message to the server using a POST request.
+Listens for form submission events, captures user input, and sends the message to the server.
+Displays the sent message in real-time on the web page.
+# Server-Side JavaScript (server.js)
+Sets up an Express.js server to handle incoming HTTP requests.
+Configures middleware for parsing JSON requests and enabling CORS.
+Defines a POST route (/send-message) that receives messages from the client.
+Inserts received messages into a MySQL database using parameterized queries.
+Responds to the client with a success message or an error message.
+Dependencies
+Express.js: A web application framework for Node.js.
+mysql2: A MySQL library for Node.js.
+dotenv: Loads environment variables from a .env file.
+cors: Middleware for enabling Cross-Origin Resource Sharing (CORS).
+
+
+# Authors
+Aldas: Enjoy using your chat app! 
+
+
+
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mysql = require("mysql2/promise");
 require("dotenv").config();
-const cors = require('cors'); 
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT;
 
@@ -17,83 +17,34 @@ app.use(bodyParser.json());
 
 app.use(cors());
 
-app.post("/send-message", (req, res) => {
+app.post("/send-message", async (req, res) => {
   const { message } = req.body;
 
   console.log("Received message from client:", message);
 
-  res.json({ status: "Message received successfully..." });
+  try {
+    const connection = await pool.getConnection();
+
+    // Insert the message into the database
+    const [result] = await connection.execute(
+      "INSERT INTO chat_app_db (chat_id, chat_msg) VALUES (NULL, ?)",
+      [message],
+    );
+
+    connection.release();
+
+    res.json({ status: "Message received and inserted into the database." });
+  } catch (error) {
+    console.error("Error inserting message into the database:", error);
+    res
+      .status(500)
+      .json({ error: "An error occurred while inserting the message." });
+  }
 });
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
 });
 
-
-
-
-
-
-
-// const express = require("express");
-// const bodyParser = require("body-parser");
-// const app = express();
-// const mysql = require("mysql2/promise"); // Using promise-based MySQL
-// require("dotenv").config();
-// const cors = require('cors');
-
-// // Enable CORS for all routes
-// app.use(cors());
-
-// const port = process.env.PORT || 9000;
-
-// // Create a MySQL pool for database connections
-// const pool = mysql.createPool({
-//   host: process.env.DB_HOST,
-//   user: process.env.DB_USER,
-//   password: process.env.DB_PASSWORD,
-//   database: process.env.DB_DATABASE,
-// });
-
-// app.use(bodyParser.json());
-
-// // Define a route to handle saving messages
-// app.post('http://localhost:9000/api/send-message', async (req, res) => {
-//   try {
-//     const userMessage = req.body.message;
-
-//     // Check if userMessage is defined and not empty
-//     if (userMessage !== undefined && userMessage !== '') {
-//       // Insert the message into the database
-//       const [result] = await pool.execute(
-//         "INSERT INTO chat_app_db (chat_id, chat_msg) VALUES (NULL, ?)",
-//         [userMessage]
-//       );
-//       console.log("Query result:", result);
-//       if (result.affectedRows === 1) {
-//         res.status(200).json({ message: "Message saved successfully", userMessage });
-//       } else {
-//         console.error("Error saving message:", result.message);
-//         res.status(500).json({ error: "Internal Server Error" });
-//       }
-//     } else {
-//       // Handle the case where userMessage is empty or undefined
-//       console.error("userMessage is empty or undefined");
-//       res.status(400).json({ error: "Bad Request" });
-//     }
-//   } catch (error) {
-//     console.error("Error saving message:", error);
-//     res.status(500).json({ error: "Internal Server Error" });
-//   }
-// });
-
-
-// app.listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-// });
-
-
-// http://localhost:9000
+// http://localhost:3000
 // node server.js
-
-
